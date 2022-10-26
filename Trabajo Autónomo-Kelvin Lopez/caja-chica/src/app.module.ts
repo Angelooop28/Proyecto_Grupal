dc09c9c61e4b68d7febc64cb20e64fd08b97bcad
kmlq12425@gmail.com <kmlq12425@gmail.com>
2022-10-25 23:27:16 -0500

    Creacion del proyecto, estaticos html y conexion de Base de Datos MongoDb

---
+++ Trabajo Autónomo-Kelvin Lopez/caja-chica/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+
+
+@Module({
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname,"..","public"),
+    
+    }),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
