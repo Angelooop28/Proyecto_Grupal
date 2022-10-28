cc9fa068bdda9b9cd60e7d9f564c60c745b32d1d
MichaelPp2020 <106557076+MichaelPp2020@users.noreply.github.com>
2022-10-27 20:43:00 -0500

    v4
    correcciones de Código

--- trabajo_autónomo-palacios_michael/src/idioma/dto/idioma.dto.ts
+++ trabajo_autónomo-palacios_michael/src/idioma/dto/idioma.dto.ts
@@ -1,8 +1,5 @@
 export class CreateIdiomaDTO{
-    readonly id: string;
+    
     readonly idIdioma: string;
-    readonly idPalabra : string;
-    readonly Deletreo : string;
-    readonly silabas : string;
-    readonly fonetica : string;
+    readonly description: string;
 }
