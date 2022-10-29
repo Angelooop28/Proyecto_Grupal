2aaf9097ea50757f6d760b615a9dc1d4c8d0885b
MichaelPp2020 <106557076+MichaelPp2020@users.noreply.github.com>
2022-10-29 08:28:06 -0500

    v8
    implementacion en   Swaggger

--- trabajo_autónomo-palacios_michael/src/main.ts
+++ trabajo_autónomo-palacios_michael/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options= new DocumentBuilder()
+  .setTitle('REGISTRO_IDIOMA')
+  .setDescription('REGISTRO DE  IDIOMA , PALABRA , REGISTRO')
+  .setVersion('1.0')
+  .addTag('REGISTRO_IDIOMA')
+  .build();
+
+const document = SwaggerModule.createDocument(app,options);
+SwaggerModule.setup('api/docs',app,document)
+
   await app.listen(3000);
 }
 bootstrap();
